396ed7d348e33c3105c5f8c926bb547263f282f0
tiago-firmino <101958346+tiago-firmino@users.noreply.github.com>
2023-05-29 20:08:58 +0100

    datatypes comments added

--- schema.sql
+++ schema.sql
@@ -22,19 +22,19 @@ DROP TABLE IF EXISTS delivery CASCADE;
 --   3. uc_table_atributes for constraints of key pairs
 
 CREATE TABLE customer (
-    cust_no             NUMERIC(9),
+    cust_no             INTEGER,                               -- datatype NUMERIC(9) so the customer identifier is a number with appropriate precision and scale with a maximum of 9 digits, which occupies
     "name"              VARCHAR(80)     NOT NULL,
     email               VARCHAR(254)    NOT NULL UNIQUE,
-    phone               VARCHAR(15)     NOT NULL,
+    phone               VARCHAR(15)     NOT NULL,              -- datatype VARCHAR(15) that uses the american convention: enclose the area code in parentheses, and then hyphenate the three-digit exchange code with the four-digit number.
     "address"           VARCHAR(255)    NOT NULL,
     CONSTRAINT pk_customer PRIMARY KEY (cust_no)
 );
 
 
 CREATE TABLE "order" (
-    order_no            NUMERIC(9),
+    order_no            INTEGER,                               -- datatype NUMERIC(9) so the order identifier is a number with appropriate precision and scale with a maximum of 9 digits
     "date"              DATE            NOT NULL,
-    cust_no             NUMERIC(9)      NOT NULL,
+    cust_no             INTEGER      NOT NULL,
     CONSTRAINT pk_order PRIMARY KEY (order_no),
     CONSTRAINT fk_order_customer FOREIGN KEY (cust_no) REFERENCES customer(cust_no),
     CONSTRAINT uc_order_cust_no UNIQUE (order_no, cust_no)
@@ -42,22 +42,22 @@ CREATE TABLE "order" (
 );
 
 CREATE TABLE sale (
-    order_no            NUMERIC(9),
+    order_no            INTEGER,
     CONSTRAINT pk_sale PRIMARY KEY (order_no),
     CONSTRAINT fk_sale_order FOREIGN KEY (order_no) REFERENCES "order" (order_no)
 );
 
 CREATE TABLE employee (
-    ssn                 NUMERIC(11),
-    TIN                 VARCHAR(20)     NOT NULL UNIQUE,
-    bdate               DATE            NOT NULL,
+    ssn                 NUMERIC(9),                            -- datatype NUMERIC(9) because of the convention: the ssn data type is used for columns holding 9-digit social security numbers where "-" are excluded (Example: 999999999)
+    TIN                 VARCHAR(20)     NOT NULL UNIQUE,       -- datatype VARCHAR(20) used to be compatible with a large amount of countries' TIN
+    bdate               DATE            NOT NULL,       
     "name"              VARCHAR(80)     NOT NULL,
     CONSTRAINT pk_employee PRIMARY KEY (ssn)
     -- Every employee (ssn) must participate in the works association
 );
 
 CREATE TABLE department (
-    "name"              VARCHAR(200),
+    "name"              VARCHAR(200),                          -- datatype VARCHAR(200) because it references a Company(Organisation) Name convention
     CONSTRAINT pk_department PRIMARY KEY ("name")
 );
 
@@ -82,7 +82,7 @@ CREATE TABLE warehouse (
 );
 
 CREATE TABLE product (
-    sku                 VARCHAR(10),
+    sku                 VARCHAR(10),                           -- datatype VARCHAR(10) because sku is usually an alphanumeric code of 8 to 10 characters
     "name"              VARCHAR(50)     NOT NULL,
     "description"       TEXT            NOT NULL,
     price               NUMERIC(16,4)   NOT NULL CHECK (price > 0),
@@ -92,13 +92,13 @@ CREATE TABLE product (
 
 CREATE TABLE ean_product (
     sku                 VARCHAR(10),
-    ean                 NUMERIC(13)     NOT NULL,
+    ean                 NUMERIC(13)     NOT NULL,              -- datatype NUMERIC(13) because an ean code consists of 13 digits that consist of a combination of country code, company code and article number. The last digit is the check digit.
     CONSTRAINT pk_ean_product PRIMARY KEY (sku),
     CONSTRAINT fk_ean_product FOREIGN KEY (sku) REFERENCES product (sku)
 );
 
 CREATE TABLE supplier (
-    TIN                 VARCHAR(20),
+    TIN                 VARCHAR(20),                           -- datatype VARCHAR(20) used to be compatible with a large amount of countries' TIN
     "name"              VARCHAR(80)     NOT NULL,
     "address"           VARCHAR(255)    NOT NULL,
     sku                 VARCHAR(10)     NOT NULL,
@@ -109,8 +109,8 @@ CREATE TABLE supplier (
 );
 
 CREATE TABLE pay (
-    cust_no             NUMERIC(9)      NOT NULL,
-    order_no            NUMERIC(9),
+    cust_no             INTEGER      NOT NULL,
+    order_no            INTEGER,
     CONSTRAINT pk_pay PRIMARY KEY (order_no),
     CONSTRAINT fk_pay_customer FOREIGN KEY (cust_no) REFERENCES customer (cust_no),
     CONSTRAINT fk_pay_order FOREIGN KEY (order_no) REFERENCES "order" (order_no),
@@ -118,24 +118,24 @@ CREATE TABLE pay (
 );
 
 CREATE TABLE process (
-    ssn                 NUMERIC(11),
-    order_no            NUMERIC(9),
+    ssn                 NUMERIC(9),
+    order_no            INTEGER,
     CONSTRAINT pk_process PRIMARY KEY (ssn, order_no),
     CONSTRAINT fk_process_employee FOREIGN KEY (ssn) REFERENCES employee (ssn),
     CONSTRAINT fk_process_order FOREIGN KEY (order_no) REFERENCES "order" (order_no)
 );
 
 CREATE TABLE contains (
-    order_no            NUMERIC(9),
+    order_no            INTEGER,
     sku                 VARCHAR(10),
-    qty                 SMALLINT        NOT NULL CHECK (qty > 0),
+    qty                 SMALLINT        NOT NULL CHECK (qty > 0), -- datatype SMALLINT because the datatype's limit is what we set to be the database's quantity maximum value
     CONSTRAINT pk_contains PRIMARY KEY (order_no, sku),                      
     CONSTRAINT fk_contains_product FOREIGN KEY (sku) REFERENCES product (sku),
     CONSTRAINT fk_contains_order FOREIGN KEY (order_no) REFERENCES "order" (order_no)
 );
 
 CREATE TABLE works (
-    ssn                 NUMERIC(11),
+    ssn                 NUMERIC(9),
     "name"              VARCHAR(200),
     "address"           VARCHAR(255),
     CONSTRAINT pk_works PRIMARY KEY (ssn, "name", "address"),
