8dc201689d25bd2dbc7ce933c91c2c2a0d619f49
tiago-firmino <tiagofirmino67@gmail.com>
2023-06-07 23:28:33 +0100

    + view

---
+++ Part 3/view.sql
@@ -0,0 +1,16 @@
+CREATE VIEW product_sales(SKU, order_no, qty, total_price, year, month, day_of_month, day_of_week, city)
+AS 
+SELECT p.SKU,
+       o.order_no,
+       c.qty,
+       c.qty * p.price AS total_price
+       EXTRACT(YEAR FROM o.date) AS year,
+       EXTRACT(MONTH FROM o.date) AS month,
+       EXTRACT(DAY FROM o.date) AS day_of_month,
+       to_char(o.date::date, 'D') AS day_of_week
+       SUBSTRING(address from E'\\d{4}-\\d{3} (.+)$' ) AS city
+FROM product p
+JOIN contains c ON p.SKU = c.SKU
+JOIN "order" o ON c.order_no = o.order_no
+JOIN pay pa ON pa.order_no = o.order_no
+JOIN customer cust ON pay.cust_no = cust.cust_no
