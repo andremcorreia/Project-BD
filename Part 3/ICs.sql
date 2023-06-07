75477c04dbcadad887dc130593b994cd034afb8d
CyFireEx <andrecorreialagos@gmail.com>
2023-06-08 00:06:03 +0100

    + ic 2?

--- Part 3/ICs.sql
+++ Part 3/ICs.sql
@@ -1,4 +1,8 @@
-CREATE OR REPLACE FUNCTION	chk_age_employee()	
+-- IC 1
+
+DROP TRIGGER check_employee_age ON employee [IF EXISTS]
+
+CREATE OR REPLACE FUNCTION chk_age_employee()	
 RETURNS TRIGGER AS
 $$
 BEGIN			
@@ -14,3 +18,22 @@ BEFORE INSERT OR UPDATE ON employee
 FOR EACH ROW
 EXECUTE FUNCTION chk_age_employee();
 
+-- IC 2
+
+DROP TRIGGER tg_chk_mandatory_workplace_office_warehouse ON workplace [IF EXISTS]
+
+CREATE OR REPLACE FUNCTION	chk_mandatory_workplace_office_warehouse()
+		RETURNS TRIGGER AS
+$$
+BEGIN
+	IF NEW.address NOT IN (SELECT address FROM office JOIN warehouse p ON ct.sku = p.sku) THEN
+		RAISE EXCEPTION	'The workplace at %	must be either an office or warehouse.',	
+        NEW.address;
+	END IF;
+	RETURN NEW;
+END;
+$$	LANGUAGE plpgsql;
+
+CREATE TRIGGER	tg_chk_mandatory_workplace_office_warehouse
+AFTER INSERT ON workplace
+FOR EACH ROW EXECUTE PROCEDURE chk_mandatory_workplace_office_warehouse();
