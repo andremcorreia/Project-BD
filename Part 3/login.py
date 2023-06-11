0abf050d446ba5619fdeeef797d952ba2c958024
CyFireEx <andrecorreialagos@gmail.com>
2023-06-11 21:29:30 +0100

    + merge + login.py

---
+++ Part 3/login.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+IST_ID = 'ist1102666'
+host = 'db.tecnico.ulisboa.pt'
+port = 5432
+password = 'cppz8442'
+db_name = IST_ID
+credentials = 'host={} port={} user={} password={} dbname={}'.format(host, port, IST_ID,
+password, db_name)
